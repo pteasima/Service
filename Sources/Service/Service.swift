a0e8bfa17dbf8ec8ab30c31fae5ec19b06831e7e
Petr Šíma <ptea.sima@gmail.com>
2021-08-05 19:08:55 +0200

    removed callAsFunction in favor of another subscript

--- Sources/Service/Service.swift
+++ Sources/Service/Service.swift
@@ -11,15 +11,14 @@ public extension Service {
   subscript<Action>(dynamicMember keyPath: KeyPath<Key.Value, Endpoint<Action>>) -> Action {
     endpoints[keyPath: keyPath](environment)
   }
-  
-  func callAsFunction<Action>() -> Action where Key.Value == Endpoint<Action> {
-    endpoints(environment)
-  }
 }
 
 public extension EnvironmentValues {
   subscript<Key: EnvironmentKey>(service keyPath: KeyPath<Key, Key> = \Key.self) -> Service<Key> {
     .init(environment: self, endpoints: self[Key.self])
+  }
+  subscript<Key: EnvironmentKey, Action>(service keyPath: KeyPath<Key, Key> = \Key.self) -> Action where Key.Value == Endpoint<Action> {
+    self[Key.self](self)
   }
 }
 
